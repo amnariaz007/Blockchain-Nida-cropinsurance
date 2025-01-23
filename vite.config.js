0861025fc793713f0e1693cdec2fc859f7fb24d0
amnariaz007 <amanamana606@gmail.com>
2025-01-24 00:05:47 +0500

    commit

--- vite.config.js
+++ vite.config.js
@@ -4,11 +4,8 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   resolve: {
-    alias: [{ find: "@", replacement: "/src" }],
+    alias: {
+      ethers5: "ethers",
+    },
   },
-  server: {
-    port: 5174, // Ensure this matches the port Vite is using
-    host: true // Expose to local network
-  },
- 
 });
